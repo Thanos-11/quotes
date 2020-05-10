ca13c4e7e94852e7f592e426c66bd60b6e449d0e
Ted Mwangi <tedletheorde@gmail.com>
2020-05-10 20:26:07 +0300

    made a Goal class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  quotes: string[];
+  goals: string[];
 
   constructor() {
-    this.quotes = [
+    this.goals = [
       'Believe to achieve or you never will',
       'A wise man once said nothing at all',
       'Tables turn, bridges burn, you live and learn',
