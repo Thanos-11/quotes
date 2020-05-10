b341b5a2a6f50061dc077bc4e36a49a1b5c0963b
Ted Mwangi <tedletheorde@gmail.com>
2020-05-10 20:22:01 +0300

    Added an array for the quotes using a constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Goals';
+  quotes: string[];
+
+  constructor() {
+    this.quotes = [
+      'Believe to achieve or you never will',
+      'A wise man once said nothing at all',
+      'Tables turn, bridges burn, you live and learn',
+    ];
+  }
 }
